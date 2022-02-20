6094fbc99112042d3f0238adf9745efc428cd618
ben-rochlin <brochlin91@gmail.com>
2022-02-20 14:16:49 +1100

    convert to scss/add title bar

--- pages/callapi.jsx
+++ pages/callapi.jsx
@@ -1,14 +1,18 @@
-import styles from '../styles/CallApi.module.css'
+import styles from '../styles/CallApi.module.scss'
 
 const CallApi = () => {
+
   return (
     <div className={styles.container}>
-      <header className={styles.buttonDiv}>
+      <header className={styles.header}>
+       Cors/Api test
+      </header>
+      <div className={styles.buttonDiv}>
         <button className={styles.button}>
           <h1>Api call to make</h1>
         </button>
         <input type="text" className={styles.input} />
-      </header>
+      </div>
       <div className={styles.bottomDiv}>
         <h1 className={styles.bottomHeader}>Bottom Div</h1>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 const Thingy = () => <h1>check meee out</h1>
 
