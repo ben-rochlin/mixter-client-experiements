63070eaf9343448fd3a1bbeb590f15d71a73c650
ben-rochlin <brochlin91@gmail.com>
2022-02-20 15:20:34 +1100

    added reactivity

--- pages/callapi.jsx
+++ pages/callapi.jsx
@@ -1,20 +1,45 @@
+import { useEffect, useState } from 'react'
 import styles from '../styles/CallApi.module.scss'
 
 const CallApi = () => {
 
+  const [loaded, setLoaded] = useState(false)
+  const [url, setUrl] = useState('place url here')
+  const [content, setContent] = useState('')
+
+  const handleChange = (e) => {
+    setUrl(e.target.value)
+  }
+
+  useEffect(() => {
+    console.log('hey from effect', url, loaded)
+  }, [url, loaded])
+
+  const HitApi = () => {
+    console.log(loaded, 'loaded b4')
+    setLoaded(true)
+    console.log(loaded, 'loaded after')
+  }
+
+
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-       Cors/Api test
+        Cors/Api test
       </header>
       <div className={styles.buttonDiv}>
-        <button className={styles.button}>
+        <button className={styles.button} onClick={HitApi}>
           <h1>Api call to make</h1>
         </button>
-        <input type="text" className={styles.input} />
+        <input type="text" value={url} className={styles.input} onChange={handleChange} />
       </div>
       <div className={styles.bottomDiv}>
-        <h1 className={styles.bottomHeader}>Bottom Div</h1>
+        {
+          loaded ?
+            <h1>loaded</h1> :
+            <h1 className={styles.bottomHeader}>nothing loaded</h1>
+        }
       </div>
     </div>
   )
