7a6814d612184805a6533956283370c03460dfae
ben-rochlin <brochlin91@gmail.com>
2022-03-01 17:43:55 +1100

    jus saving, nothing special

--- pages/callapi.jsx
+++ pages/callapi.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import styles from '../styles/CallApi.module.scss'
+import blip from 'utils.js'
 
 const CallApi = () => {
 
@@ -12,12 +13,15 @@ const CallApi = () => {
   }
 
   useEffect(() => {
-    console.log('hey from effect', url, loaded)
+    console.log('hey from effect', url, loaded, blip)
   }, [url, loaded])
 
-  const HitApi = () => {
-    console.log(loaded, 'loaded b4')
+  const HitApi = (e) => {
+    console.log('e', e.preventDefault())
+    // e.preventDefault()
+    console.log(loaded, 'loaded b4', blip)
     setLoaded(true)
+    setUrl(blip)
     console.log(loaded, 'loaded after')
   }
 
@@ -40,6 +44,9 @@ const CallApi = () => {
             <h1>loaded</h1> :
             <h1 className={styles.bottomHeader}>nothing loaded</h1>
         }
+      </div>
+      <div className={styles.bottomDiv}>
+        <h1>{url}</h1>
       </div>
     </div>
   )
